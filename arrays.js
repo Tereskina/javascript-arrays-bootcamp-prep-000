a11d149aad93857c60e8a41e4b2f56d221fd1972
Kseniia Tereskina <iv.ksenia@gmail.com>
2018-07-05 20:30:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+return chocolateBars
+
+var addElementToBeginningOfArray = ['tea', 'sugar']
+addElementToBeginningOfArray.unshift('water')
+var destructivelyAddElementToBeginningOfArray = destructivelyAddElementToBeginningOfArray.slice(1)
+
+var addElementToEndOfArray = ['bread', 'butter']
+addElementToEndOfArray.push('cheese')
+var destructivelyAddElementToEndOfArray = []
+
+var accessElementInArray = ['Hugh Laurie', 'Damian Lewis', 'Angelina Jolie'];
+return (acessElementInArray[0])
+
+var destructivelyRemoveElementFromBeginningOfArray = ['a', 'b', 'c']
+return destructivelyRemoveElementFromBeginningOfArray('a')
+var removeElementFromBeginningOfArray = ['q', 'w','e']
